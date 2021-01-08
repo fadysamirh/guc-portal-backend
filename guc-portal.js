cc7f46c5222939844a40f071aa1621886bad41d2
Fady <fadysamirh@gmail.com>
2021-01-08 18:59:37 +0200

    fixes

--- guc-portal.js
+++ guc-portal.js
@@ -72,5 +72,5 @@ app.use((req, res, next) => {
   next()
 })
 connectDB()
-const port = 3000
+const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Server up and running on ${port}`))
